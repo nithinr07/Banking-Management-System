45bdd91d6158cb4bd8400cc9c639682ac64499d4
Nithin Raj <nithin.raj@iiitb.org>
2020-05-27 09:01:30 +0530

    Modified seed program

--- src/seed.c
+++ src/seed.c
@@ -21,19 +21,23 @@ int main() {
         .status = "ACTIVE"
     };
     write(fd1, &u1, sizeof(u1));
+    printf("********Normal User seed********\n");
+    printf("id=%d\tname=%s\tpassword=%s\n",u1.userID, u1.name, u1.password);
     close(fd1);
 
     int fd2 = open(JOINTUSER_DB_PATH, O_RDWR | O_CREAT, 0744);
     jointUser u2 = {
         .userID = 1,
-        .name1 = "ABCD",
-        .name2 = "Raj",
+        .name1 = "Crazy",
+        .name2 = "Frog",
         .password = "1234",
         .account_no = 2000,
         .balance = 1000.0,
         .status = "ACTIVE"
     };
     write(fd2, &u2, sizeof(u2));
+    printf("********Joint User seed********\n");
+    printf("id=%d\tname1=%s\tname2=%s\tpassword=%s\n",u2.userID, u2.name1, u2.name2, u2.password);
     close(fd2);
 
     int fd3 = open(ADMIN_DB_PATH, O_RDWR | O_CREAT, 0744);
@@ -43,6 +47,8 @@ int main() {
         .password = "1234"
     };
     write(fd3, &u3, sizeof(u3));
+    printf("********Admin seed********\n");
+    printf("id=%d\tusername=%s\tpassword=%s\n",u3.userID, u3.username, u3.password);
     close(fd3);
     return 0;
 }
